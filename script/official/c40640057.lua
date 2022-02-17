3a30b552f6e00932f554693dd09ec7605e9304ed
raiderpls <89932011+raiderpls@users.noreply.github.com>
2022-02-17 01:25:25 -0500

    Add files via upload

---
+++ script/official/c40640057.lua
@@ -0,0 +1,31 @@
+--クリボー
+--Kuriboh
+local s,id=GetID()
+function s.initial_effect(c)
+	--no damage
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCode(EVENT_PRE_DAMAGE_CALCULATE)
+	e1:SetCondition(s.con)
+	e1:SetCost(s.cost)
+	e1:SetOperation(s.op)
+	c:RegisterEffect(e1)
+end
+function s.con(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetTurnPlayer()~=tp and Duel.GetBattleDamage(tp)>0
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsDiscardable() end
+	Duel.SendtoGrave(e:GetHandler(),REASON_COST+REASON_DISCARD)
+end
+function s.op(e,tp,eg,ep,ev,re,r,rp)
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_AVOID_BATTLE_DAMAGE)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetTargetRange(1,0)
+	e1:SetReset(RESET_PHASE+PHASE_DAMAGE)
+	Duel.RegisterEffect(e1,tp)
+end
