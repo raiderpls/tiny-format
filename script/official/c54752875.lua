60cbfecb3f69ac3f8c0764cd0656e2ccc29aaebe
raiderpls <89932011+raiderpls@users.noreply.github.com>
2022-02-16 21:35:25 -0500

    Add files via upload

--- script/official/c54752875.lua
+++ script/official/c54752875.lua
@@ -1,7 +1,7 @@
 --双頭の雷龍
 local s,id=GetID()
 function s.initial_effect(c)
-    --fusion material
-    c:EnableReviveLimit()
-    Fusion.AddProcMixN(c,true,true,31786629,2)
+	--fusion material
+	c:EnableReviveLimit()
+	Fusion.AddProcMixN(c,true,true,31786629,2)
 end
