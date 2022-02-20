be2d0ecb1d5645d33b150ce2d14104c371568f19
raiderpls <89932011+raiderpls@users.noreply.github.com>
2022-02-19 21:28:50 -0500

    Add files via upload

---
+++ script/official/c35752363.lua
@@ -0,0 +1,7 @@
+--朱雀
+local s,id=GetID()
+function s.initial_effect(c)
+	--fusion material
+	c:EnableReviveLimit()
+	Fusion.AddProcMix(c,true,true,62403074,71407486)
+end
---
+++ script/official/c80516007.lua
@@ -0,0 +1,7 @@
+--レア・フィッシュ
+local s,id=GetID()
+function s.initial_effect(c)
+	--fusion material
+	c:EnableReviveLimit()
+	Fusion.AddProcMix(c,true,true,1641882,75376965)
+end
